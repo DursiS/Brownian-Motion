205d6f1e15777382423966bd0bc199eea3bb5c5d
Sean Dursi <seanrobertdursi@gmail.com>
2026-05-02 19:24:16 -0400

    Added GBM

--- BM-GBM.py
+++ BM-GBM.py
@@ -1,5 +1,6 @@
 import Discrete_Distributions
 import matplotlib.pyplot as plt
+import math
 
 
 class BrownianMotion:
@@ -68,7 +69,9 @@ class BrownianMotion:
     def expectation(self) -> float:
         """Return the expected final height given
         len(self.steps) steps were taken."""
-        pass
+
+        time_passed = self._steps_taken * self.time_step
+        return self.mu * time_passed
 
     def var(self) -> float:
         """Return the variance of this random walk"""
@@ -80,9 +83,9 @@ class BrownianMotion:
 
     def visualize(self) -> None:
         """Plot this BrownianMotion."""
-        x = [self.time_step * i for i in range(self._steps_taken)]
+        x = [item[0] for item in self.path]
         y = [item[1] for item in self.path]
-        plt.plot(x, y[:-1])
+        plt.plot(x, y)
 
     def visualize_stats(self) -> None:
         """Plot the stats for the <number>th RandomWalk after <n> steps."""
@@ -90,18 +93,64 @@ class BrownianMotion:
         x1 = [item[0] for item in self.path]
         expt = self.expectation()
         y2 = [expt for i in range(self._steps_taken)]
-        plt.plot(x1[:-1], y2, c="#00008B", label=f"Mean")
+        name = self.__class__.__name__
+        plt.plot(x1[:-1], y2, c="#00008B", label=f"Expected End {name}")
 
         # Root and Legend
-        x2 = [i for i in range(self._steps_taken)]
-        y2 = [self._steps_taken ** (1 / 2) for i in range(self._steps_taken)]
-        y3 = [-(self._steps_taken ** (1 / 2)) for i in range(self._steps_taken)]
-        plt.plot(x2, y2, c="#FF0000", label="Root(n)")
-        plt.plot(x2, y3, c="#FF0000")
+        time_passed = self._steps_taken * self.time_step
+        y2 = [time_passed ** (1 / 2) for i in range(self._steps_taken)]
+        y3 = [-(time_passed ** (1 / 2)) for i in range(self._steps_taken)]
+        plt.plot(x1[:-1], y2, c="#FF0000", label="Root(t)")
+        plt.plot(x1[:-1], y3, c="#FF0000")
+
+
+class GBrownianMotion(BrownianMotion):
+
+    def __init__(
+        self,
+        theta: float,
+        h_step: float,
+        time_step: float,
+        mu: float = 0,
+        sigma: float = 1,
+    ) -> None:
+        """Create a new BrownianMotion.
+
+        Preconditions:
+            - h_step > 0
+            - time_step > 0
+            - theta > 0
+        """
+        super().__init__(theta, h_step, time_step, mu, sigma)
+        self.position = 0, 1
+
+    def step(self) -> None:
+        """Move one time_step forward"""
+
+        shock = Discrete_Distributions.Normal(0, 1)
+        noise = self.sigma * (self.time_step ** (1 / 2)) * shock.sample()
+        step_factor = math.exp(
+            (self.mu - (1 / 2) * (self.sigma**2)) * self.time_step + noise
+        )
+
+        new_pos = (self.position[0] + self.time_step), (self.position[1] * step_factor)
+        self.position = new_pos
+        self.path.append(new_pos)
+        self._steps_taken += 1
 
 
 if __name__ == "__main__":
-    bm = BrownianMotion(1 / 3, 0.001, 0.001)
-    bm.run(10000)
+    bm = BrownianMotion(1 / 2, 0.001, 0.001)
+    gbm = GBrownianMotion(1 / 2, 0.001, 0.001)
+    n = 10000
+
+    bm.run(n)
     bm.visualize()
+    bm.visualize_stats()
+
+    gbm.run(n)
+    gbm.visualize()
+    gbm.visualize_stats()
+
+    plt.legend()
     plt.show()
